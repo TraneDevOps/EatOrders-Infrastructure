a26f0e72cfb0f00e4c3588e227f294c8ea96eae2
TraneDevOps <tranedevops@gmail.com>
2022-07-02 16:06:22 -0700

    terraform

--- gcp-setup/memorystore.tf
+++ gcp-setup/memorystore.tf
@@ -7,25 +7,25 @@ module "memorystore" {
   region                  = var.region
   authorized_network      = var.vpc_name
   transit_encryption_mode = "DISABLED"
-  depends_on = [google_project_service.enabled_apis]
-} 
+  depends_on              = [google_project_service.enabled_apis]
+}
 data "google_client_config" "default" {}
 
 
 provider "helm" {
-    kubernetes {
-        host                   = "https://${module.gke-cluster.endpoint}"
-        token                  = data.google_client_config.default.access_token
-        cluster_ca_certificate = base64decode(module.gke-cluster.ca_certificate)
-    }
+  kubernetes {
+    host                   = "https://${module.gke-cluster.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(module.gke-cluster.ca_certificate)
+  }
 }
 
-# resource "helm_release" "redis_ip" {
-#   name    = "redis-ip"
-#   chart   = "./helm/redis_ip"
-#   version = "2.1.2"
-#   set{
-#       name = "redis_ip"
-#       value = module.memorystore.host
-#   }
-# }
+resource "helm_release" "redis_ip" {
+  name    = "redis-ip"
+  chart   = "./helm/redis_ip"
+  version = "2.1.2"
+  set {
+    name  = "redis_ip"
+    value = module.memorystore.host
+  }
+}
