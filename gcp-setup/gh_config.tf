8c0e46ef18000cd58a142231a5600e7ed9b82ef0
TraneDevOps <tranedevops@gmail.com>
2022-07-02 22:21:08 -0700

    terraform

--- gcp-setup/gh_config.tf
+++ gcp-setup/gh_config.tf
@@ -7,10 +7,10 @@ resource "github_actions_secret" "gh_infra_sa_key" {
   depends_on      = [google_service_account_key.service_account_key]
 }
 
-# resource "github_actions_secret" "gh_ms_sa_key" {
-#   repository      = var.gh_microservices_repo
-#   secret_name     = var.gh_secret_name
-#   plaintext_value = google_service_account_key.service_account_key.private_key
-#   depends_on      = [google_service_account_key.service_account_key]
-# }
+resource "github_actions_secret" "gh_ms_sa_key" {
+  repository      = var.gh_microservices_repo
+  secret_name     = var.gh_secret_name
+  plaintext_value = google_service_account_key.service_account_key.private_key
+  depends_on      = [google_service_account_key.service_account_key]
+}
 
