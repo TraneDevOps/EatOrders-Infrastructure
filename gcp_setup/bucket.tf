f2d8f092de524d4d3288927df6fb2af91d0fa807
TraneDevOps <tranedevops@gmail.com>
2022-07-03 19:21:03 -0700

    terraform

--- gcp_setup/bucket.tf
+++ gcp_setup/bucket.tf
@@ -1,6 +1,7 @@
 # create bucket for terraform setup for microservices
 resource "google_storage_bucket" "tf_microservices_bucket" {
-  name          = var.microservices_bucket
-  location      = var.location
-  force_destroy = true
+  name                        = var.microservices_bucket
+  location                    = var.location
+  force_destroy               = true
+  uniform_bucket_level_access = true
 }
